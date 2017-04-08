f8082fbbba2d7bd81e9d263e3a5fad8aa5b18f0b
locta66 <785647081@qq.com>
2017-04-08 10:29:11 +0800

    Initial commit

---
+++ src/Calculate.java
@@ -0,0 +1,10 @@
+/**
+ * Created by yingyue on 2017/3/23.
+ */
+public class Calculate {
+    public static void main(String args[]){
+        double credits =1 +3;
+        double grades = 4+9.6+2.7+6.2+6.6+3.3+3.3;
+                System.out.println(grades/credits);
+    }
+}
---
+++ src/NewIndexFiles.java
@@ -0,0 +1,26 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+
+/**
+ * Created by yingyue on 2017/4/8.
+ */
+public class NewIndexFiles {
+    private IndexWriter idw;
+    private IndexWriterConfig idwConfig = new IndexWriterConfig(new StandardAnalyzer());
+
+    public void getIndexWriter(){
+
+
+    }
+
+    public void IndexFiles(){
+
+
+
+    }
+
+    public static void main(String argvs[]){
+
+    }
+}
